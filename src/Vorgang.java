324351d8de74017b36b50366fc5c51f9840f11c3
l4h7 <lucashufeland@outlook.com>
2020-10-29 07:58:15 +0100

    initial commit

---
+++ src/Vorgang.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Vorgang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        int counter = 0;
+        String[] stati = {"n", "b", "s", "a", "t", "x"};
+        String[] statiDefinition = {
+                "Neuen Vorgang öffnen",
+                "Vorgang abverfügt",
+                "Vorgang geschlossen",
+                "Vorgang archiviert",
+                "Aktueller Status des Vorgangs",
+                "Beenden"
+        };
+
+        while (!input.equals("x")) {
+            System.out.println("Status eingeben: ");
+
+            input = sc.next().toLowerCase();
+
+            if (input.equals("t") && counter > 0) {
+                System.out.println("Aktueller Status des Vorgangs: " + statiDefinition[counter - 1]);
+            } else {
+                if (!stati[counter].equals(input)) {
+                    if (input.equals("x")) {
+                        counter = 5;
+                        System.out.println(statiDefinition[counter]);
+                    } else {
+                        System.out.println("FALSCHE EINGABE! Bitte Nochmal.");
+                    }
+
+                } else {
+                    System.out.println(statiDefinition[counter]);
+                    counter++;
+                }
+            }
+        }
+    }
+}
